a26bf4a8237243cd1231146d29c9f3204d981533
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-14 16:48:08 +0530

    Add

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -11,11 +11,20 @@ namespace UserRegistration
             Console.WriteLine("\n==============||===============\n");
 
             Registration reg = new Registration();
-           // reg.Register();
+            // reg.Register();
             //reg.EmailRegistration();
-           // reg.MobileRegistration();
-           // reg.PasswordRegistration();
-            reg.PasswordRegistration();
+            // reg.MobileRegistration();
+            // reg.PasswordRegistration();
+            // reg.PasswordRegistration();
+            reg.CheckEmail("abc@yahoo.com");
+            reg.CheckEmail("abc-100@yahoo.com");
+            reg.CheckEmail("abc.100@yahoo.com");
+            reg.CheckEmail("abc111@abc.com");
+            reg.CheckEmail("abc-100@abc.net");
+            reg.CheckEmail("abc.100@abc.com.au");
+            reg.CheckEmail("abc@1.com");
+            reg.CheckEmail("abc@gmail.com.com");
+            reg.CheckEmail("abc+100@gmail.com");
         }
     }
 }
